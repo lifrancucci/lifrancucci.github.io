d7c5d9ac25b3b44206d2c9b8a1624f5d6f089e57
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-04-18 13:10:19 -0300

    Add code to preload large version of images

--- js/gallery.js
+++ js/gallery.js
@@ -6,15 +6,15 @@ function activateGallery() {
   let thumbnails  = document.querySelectorAll("#gallery-thumbs > div > img");
   let mainImage   = document.querySelector("#gallery-photo > img");
                                     // Or ("#gallery-photo img");
-  // Image info to be updated
-  let galleryInfo = document.querySelector("#gallery-info");
-  let title       = galleryInfo.querySelector(".title");
-  let description = galleryInfo.querySelector(".description");
 
   thumbnails.forEach(function(thumbnail) {
+    // Preload large images
+    let newImageSrc   = thumbnail.dataset.largeVersion;
+    let largeVersion  = new Image();
+    largeVersion.src  = newImageSrc;
+
     thumbnail.addEventListener("click", function() {
       // Set clicked image as main image
-      let newImageSrc = thumbnail.dataset.largeVersion;
       mainImage.setAttribute("src", newImageSrc);
       mainImage.setAttribute("alt", thumbnail.alt);
 
@@ -23,10 +23,14 @@ function activateGallery() {
       document.querySelector(".current").classList.remove(currentClass);
       thumbnail.parentNode.classList.add(currentClass);
 
+      // Image info to be updated
+      let galleryInfo = document.querySelector("#gallery-info");
+      let title       = galleryInfo.querySelector(".title");
+      let description = galleryInfo.querySelector(".description");
+
       // Change image title and description in third column
       title.innerHTML       = thumbnail.dataset.title;
       description.innerHTML = thumbnail.dataset.description;
-
     });
   });
 }
