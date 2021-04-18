4e0cdc889b61e5e88e7fb4aebcb638a3d9275178
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-04-18 13:03:20 -0300

    Finish the JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,32 @@
+// Activates the image gallery
+// The main task is to attach an event listener to each image in the gallery
+// and respond appropriately on click
+
 function activateGallery() {
-  alert("Hello from the gallery file!");
+  let thumbnails  = document.querySelectorAll("#gallery-thumbs > div > img");
+  let mainImage   = document.querySelector("#gallery-photo > img");
+                                    // Or ("#gallery-photo img");
+  // Image info to be updated
+  let galleryInfo = document.querySelector("#gallery-info");
+  let title       = galleryInfo.querySelector(".title");
+  let description = galleryInfo.querySelector(".description");
+
+  thumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener("click", function() {
+      // Set clicked image as main image
+      let newImageSrc = thumbnail.dataset.largeVersion;
+      mainImage.setAttribute("src", newImageSrc);
+      mainImage.setAttribute("alt", thumbnail.alt);
+
+      // Change which image is current
+      let currentClass = "current"
+      document.querySelector(".current").classList.remove(currentClass);
+      thumbnail.parentNode.classList.add(currentClass);
+
+      // Change image title and description in third column
+      title.innerHTML       = thumbnail.dataset.title;
+      description.innerHTML = thumbnail.dataset.description;
+
+    });
+  });
 }
