b7b7f3890ce3ac8cfe300fa42c10bd9ddc3ab1ab
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-08-11 10:04:43 -0300

    Change header layout and bkg image

---
+++ js/main.js
@@ -0,0 +1,23 @@
+function contentLoaded() {
+
+  // HEADER
+  let title = document.getElementById("#title");
+  let portfolio = document.getElementById("#portfolio");
+
+  // FRAME COUNTER
+  // const times = [];
+  // let fps;
+  //
+  // function refreshLoop() {
+  //   window.requestAnimationFrame(() => {
+  //     const now = performance.now();
+  //     while (times.length > 0 && times[0] <= now - 1000) {
+  //       times.shift();
+  //     }
+  //     times.push(now);
+  //     fps = times.length;
+  //     refreshLoop();
+  //   });
+  // }
+  // refreshLoop();
+}
