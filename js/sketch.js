721702a2f61f937a582696cdd185d8de84208c99
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-08-12 16:01:34 -0300

    Define the header and pointer effect

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,16 @@
 let colorFg = "#111111";
 let colorBg = "#f1f1f1";
-let colorTxt = "#ffbc12"
+let colorTxt = "#d90921"
 
 let imgBkg; // Background image
 let imgFont; // For typography
-let pg;
+let pgTxt;
+
+let maxDistance;
+let tilesX;
+let tilesY;
+let tileSize
+// -----------------------------------------------
 
 function preload() {
   imgBkg = loadImage("./img/sea_sq.jpg");
@@ -12,11 +18,17 @@ function preload() {
 }
 
 function setup() {
+  frameRate(30);
   let myCanvas = createCanvas(600, 600);
   myCanvas.parent('sketchContainer');
   background(colorBg);
-  pg = createGraphics(600, 600);
+  tilesX = 80;
+  tilesY = 1;
+  tileSize = width/tilesX;
+  pgTxt = createGraphics(600, 600);
+  pgWaves = createGraphics(200, 200);
   imgBkg.resize(600, 600);
+  maxDistance = dist(0, 0, width, height);
 }
 
 function draw() {
@@ -26,35 +38,37 @@ function draw() {
   fill(colorFg);
   noStroke();
 
-  let tilesX = 120;
-  let tilesY = tilesX;
-  let tileSize = width/tilesX;
   for(let y = 0; y < imgBkg.height; y += tileSize) {
     for(let x = 0; x < imgBkg.width; x += tileSize) {
       let pixelColor = imgBkg.get(x, y);
       let b = map(brightness(pixelColor), 0, 255, 1, 0);
 
+      let mouseSize = dist(mouseX, mouseY, x, y);
+      mouseSize = mouseSize/maxDistance  * -200;
+
       // Push the tile to the matrix
       push();
       translate(x + tileSize/2, y + tileSize/2);
-      ellipse(0, 0, b * tileSize, b * tileSize);
+      ellipse(0, 0, b * tileSize, (b * tileSize) + mouseSize/1.8);
       pop(); // Pop it at the end
     }
   }
 
   // Text:
-  // pg.background(0, 0);
-  pg.fill(colorTxt);
-  pg.textFont(imgFont);
-  pg.textSize(120);
-  pg.push();
-  pg.translate(20, height/2-110);
-  pg.textAlign(LEFT, LEFT);
-  pg.text("LI",0, 0);
-  pg.text("FRAN",0, 100);
-  pg.text("CU",0, 200);
-  pg.text("CCI",0, 300);
-  pg.pop();
-  image(pg,0,0);
+  pgTxt.fill(colorTxt);
+  pgTxt.textFont(imgFont);
+  pgTxt.push();
+  pgTxt.translate(20, height/2-110);
+  pgTxt.textAlign(LEFT, BOTTOM);
+  pgTxt.textSize(120);
+  pgTxt.text("LI",0, 0);
+  pgTxt.text("FRAN",0, 100);
+  pgTxt.text("CU",0, 200);
+  pgTxt.text("CCI",0, 300);
+  pgTxt.textSize(40);
+  pgTxt.text("DESARROLLO CREATIVO", 6, 310);
+  pgTxt.pop();
+  image(pgTxt,0,0); // Show the text
 
+  print(frameCount);
 }
