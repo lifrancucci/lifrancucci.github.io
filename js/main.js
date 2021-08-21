313a0164f3009848890db3338159e4350c1c9a6e
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-08-21 20:11:16 -0300

    Delete unused files, remove lang bar

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
 function contentLoaded() {
 
-  // HEADER
-  let title = document.getElementById("#title");
-  let portfolio = document.getElementById("#portfolio");
-
-  // FRAME COUNTER
-  // const times = [];
-  // let fps;
-  //
-  // function refreshLoop() {
-  //   window.requestAnimationFrame(() => {
-  //     const now = performance.now();
-  //     while (times.length > 0 && times[0] <= now - 1000) {
-  //       times.shift();
-  //     }
-  //     times.push(now);
-  //     fps = times.length;
-  //     refreshLoop();
-  //   });
-  // }
-  // refreshLoop();
 }
