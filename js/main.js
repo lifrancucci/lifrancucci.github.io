4ad6cea3db32d151aa27cdfb528f8658bffa090a
Lisandro Francucci <lisandrofrancucci@tutanota.com>
2021-08-22 13:00:31 -0300

    Add language switcher EN/ES

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 function contentLoaded() {
+  // LANG BAR
+  let switchEn = document.getElementById('switch-en');
+  let switchEs = document.getElementById('switch-es');
+  let myRoot = document.querySelector(':root');
+
+  function langEnglish() {
+    console.log("clicking on EN");
+    myRoot.style.setProperty('--lang-en', 'block');
+    myRoot.style.setProperty('--lang-es', 'none');
+  }
+  function langSpanish() {
+    console.log("clicking on ES");
+    myRoot.style.setProperty('--lang-es', 'block');
+    myRoot.style.setProperty('--lang-en', 'none');
+  }
+
+  switchEn.addEventListener('click', langEnglish);
+  switchEs.addEventListener('click', langSpanish);
 
 }
